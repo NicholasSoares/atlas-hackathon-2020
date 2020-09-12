9b52fff7cc06876e5a130cfe929e8d95dfc7a31c
NicholasSoares <42093631+NicholasSoares@users.noreply.github.com>
2020-09-11 21:21:45 -0300

    Update database.sql

--- Documents/database.sql
+++ Documents/database.sql
@@ -1,5 +1,7 @@
 drop table if EXISTS roles cascade;
 drop table if EXISTS users cascade;
+drop table if EXISTS institutions_categories cascade;
+drop table if EXISTS institutions cascade;
 drop table if EXISTS session cascade;
 
 create table roles (
@@ -19,9 +21,29 @@ create table users (
     deleted boolean not null DEFAULT false
 );
 
+create table institutions_categories (
+	institution_category_id BIGSERIAL PRIMARY KEY,
+	name VARCHAR (200) NOT NULL,
+	created_on TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted boolean not null DEFAULT false
+);
+
+create table institutions (
+	user_id BIGSERIAL PRIMARY KEY,
+	institution_category_id int NOT NULL references institutions_categories(institution_category_id),
+	endereco VARCHAR (200) NOT NULL,
+	bairro VARCHAR (200) NOT NULL,
+	cep VARCHAR (200) NOT NULL,
+    cidade VARCHAR (200) default null,
+	telefone VARCHAR (30) NOT NULL,
+	email VARCHAR (200) UNIQUE NOT NULL,
+	cnpj VARCHAR (14) UNIQUE NOT NULL,
+	created_on TIMESTAMP NOT NULL DEFAULT NOW(),
+    deleted boolean not null DEFAULT false
+);
 
 insert into roles (role_id, rolename) values (0, 'Adminstrador');
-insert into roles (role_id, rolename) values (1,'Usuario');
+insert into roles (role_id, rolename) values (1, 'Usuario');
 
 insert into users (role_id, username, email, password, cellphone) values (0, 'Adminstrador', 'teste@gmail.com', '$2b$10$hbiTjICrsPzXnsMTducHzOzvrm0L1LorrSjO3QHs32Xax0q1O8lPa','(53) 99999-9999');
 
